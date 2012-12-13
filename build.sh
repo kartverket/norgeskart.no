682b25db0a8998ee87057bd5cb53284b8f5acce5
relet <thomas.hirsch@kartverket.no>
2012-12-13 11:11:24 +0100

    basic build script - tbd: check if we'd rather use a build system

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# confirm submodules are correct, initialized and up to date - but don't commit automatically
+git submodule init   || exit 1
+git submodule update || exit 1
+
+# build every anathem configuration into htdocs
+cd lib/src/anathem
+git pull origin norgeskart || exit 1
+
+for i in $(ls themes); do 
+  export name=$(echo $i|sed -e 's/\..*//g');
+  echo "building $name..."
+  #mkdir -p ../$name
+  python26 anathem.py $name > ../../../htdocs/$name.html;
+done || exit 1
+cd ../../..
+
+# copy all anathem libraries into htdocs/js
+# -u update flag only copies newer files
+cp -u lib/src/anathem/lib/js/*.js htdocs/js/ || exit 1
+
+# build openlayers custom 
+cd lib/src/openlayers
+git pull origin master     || exit 1 
+cd build
+python build.py kartverket || exit 1
+cd ../../../..
+
+# copy openlayers build into htdocs/js
+cp lib/src/openlayers/build/OpenLayers.js htdocs/js/openlayers.min.js || exit 1
+
+
