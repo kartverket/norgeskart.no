7790cf30dcda374975411291c09091128e8d5fac
Thomas Hirsch <thomas.hirsch@kartverket.no>
2014-11-19 13:49:21 +0100

    build only ol3 branch

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ git submodule update || exit 1
 
 # build every anathem configuration into htdocs
 cd lib/src/anathem
-git pull origin norgeskart || exit 1
+git pull origin ol3 || exit 1
 
 for i in $(cd themes && ls *.yaml); do 
   export name=$(echo $i|sed -e 's/\..*//g');
@@ -23,18 +23,3 @@ mkdir -p htdocs/js
 #cp -ur lib/src/anathem/index.manifest htdocs/ || exit 1
 cp -ur lib/src/anathem/lib/js/*.* htdocs/js/ || exit 1
 cp -ur lib/src/sosi.js/dist/SOSI.min.js htdocs/js/ || exit 1
-cp -ur lib/src/sosi.js/lib/underscore-min.js htdocs/js/ || exit 1
-
-# build openlayers custom 
-cd lib/src/openlayers
-git pull origin master     || exit 1 
-cd build
-./build.sh kartverket || exit 1
-cd ../../../..
-
-# copy openlayers build into htdocs/js
-cp lib/src/openlayers/build/OpenLayers.js htdocs/js/openlayers.min.js || exit 1
-# copy openlayers themes into htdocs/js/openlayers/theme
-cp -r lib/src/openlayers/theme htdocs || exit 1
-cp -r lib/src/openlayers/img htdocs || exit 1
-
